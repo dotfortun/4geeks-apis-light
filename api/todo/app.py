ca0ecab3295154a0961c0a4e18053c2596cad496
Shane Bell <being.for.itself@gmail.com>
2023-12-29 15:27:05 -0500

    I have opinions about sqlmodel, but not enough to not use it yet.

--- api/todo/app.py
+++ api/todo/app.py
@@ -1,17 +1,108 @@
-from fastapi import FastAPI, Request
+import os
+
+from typing import List, Optional, Annotated
+
+from fastapi import (
+    FastAPI, Request, Response, HTTPException,
+    Query, Depends, status,
+)
+
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
-app = FastAPI()
+from sqlmodel import (
+    Session, create_engine, select
+)
+
+from .models import (
+    TodoUser, TodoUserCreate, TodoUserRead, TodoUserReadWithItems,
+    TodoItem, TodoItemCreate, TodoItemRead, TodoItemUpdate,
+    TodoUserList
+)
+
+app = FastAPI(
+    title="Todo API"
+)
 
 limiter = Limiter(key_func=get_remote_address)
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 
-@app.get("/readme")
-def readme():
+engine = create_engine(
+    os.getenv("DB_URL", "sqlite:///./playground.sqlite")
+)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@app.post(
+    "/users",
+    status_code=status.HTTP_201_CREATED,
+    response_model=TodoUserRead
+)
+@limiter.limit("15/minute")
+def create_user(
+    user: TodoUserCreate,
+    request: Request,
+    session: Session = Depends(get_session)
+) -> None:
+    user_exists = session.exec(select(TodoUser).where(
+        TodoUser.name == user.name)).first()
+    if not user_exists:
+        db_user = TodoUser.model_validate(user)
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+        return db_user
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="User already exists."
+    )
+
+
+@app.delete(
+    "/users/{user_id: int}",
+)
+@limiter.limit("15/minute")
+def delete_user(
+    *,
+    request: Request,
+    user_id: int | None = None,
+    session: Session = Depends(get_session)
+):
+    user = session.exec(select(TodoUser).where(
+        TodoUser.name == user_id)
+    ).first()
+    if user:
+        session.delete(user)
+        session.commit()
+        return Response(
+            status_code=status.HTTP_204_NO_CONTENT
+        )
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail=f"User {user_id} doesn't exist."
+    )
+
+
+@app.get(
+    "/users",
+    response_model=TodoUserList
+)
+@limiter.limit("120/minute")
+def get_users(
+    request: Request,  # This is required for slowapi.
+    offset: int = 0,
+    limit: int = Query(default=100, le=100),
+    session: Session = Depends(get_session)
+):
     return {
-        "message": "Check it out!"
+        "users": session.exec(
+            select(TodoUser).offset(offset).limit(limit)
+        ).all()
     }
--- api/todo/models.py
+++ api/todo/models.py
@@ -3,23 +3,39 @@ from typing import List, Optional
 from sqlmodel import (
     SQLModel, Field, Relationship,
 )
+from pydantic import BaseModel
 
 
 class TodoUserBase(SQLModel):
-    name: str = Field(index=True)
+    name: str = Field(
+        index=True,
+        unique=True,
+    )
 
 
 class TodoUser(TodoUserBase, table=True):
     id: Optional[int] = Field(
         default=None,
-        primary_key=True
+        primary_key=True,
     )
     todos: List["TodoItem"] = Relationship(back_populates="user")
 
 
+class TodoUserCreate(TodoUserBase):
+    name: str
+
+
+class TodoUserRead(TodoUserBase):
+    id: int
+    name: str
+
+
 class TodoItemBase(SQLModel):
-    label: str = Field(index=True)
+    label: str
     is_done: bool = Field(default=False)
+    user_id: int = Field(
+        foreign_key="todouser.id"
+    )
 
 
 class TodoItem(TodoItemBase, table=True):
@@ -28,3 +44,28 @@ class TodoItem(TodoItemBase, table=True):
         primary_key=True
     )
     user: Optional["TodoUser"] = Relationship(back_populates="todos")
+
+
+class TodoItemCreate(TodoItemBase):
+    pass
+
+
+class TodoItemRead(TodoItemBase):
+    id: int
+    label: str
+    is_done: bool
+
+
+class TodoItemUpdate(TodoItemBase):
+    label: Optional[str]
+    is_done: Optional[bool]
+
+
+# Models with relationships
+
+class TodoUserReadWithItems(TodoUserBase):
+    todos: List["TodoItemRead"]
+
+
+class TodoUserList(BaseModel):
+    users: List[TodoUserRead]
