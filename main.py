1cc30d3af8f94d66c22153a4d2b924332e28efc2
Shane Bell <being.for.itself@gmail.com>
2023-12-30 22:58:03 -0500

    Just added a basic homepage that dynamically lists apis.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import re
 
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
 import api
+
+template = None
+with open("./static/index.html", "r") as f:
+    template = f.read()
+
 
 app = FastAPI(
     title="4Geeks Playground",
@@ -13,7 +19,7 @@ app = FastAPI(
 
 for mod in api.__all__:
     name = re.sub("api\.", "", mod.__name__)
-    app.mount(f"""/{name}""", getattr(mod, "app"))
+    app.mount(f"""/{name}""", getattr(mod, "app"), name)
 
 
 limiter = Limiter(key_func=get_remote_address)
@@ -21,11 +27,15 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 
-@app.get("/")
-async def root(request: Request):
-    return {
-        "message": ""
-    }
+@app.get("/", include_in_schema=False)
+async def app_root(request: Request):
+    routes = ""
+    for mod in api.__all__:
+        name = re.sub("api\.", "", mod.__name__)
+        routes += f"""<li><a href="/{name}/docs">{name.title()}</a></li>"""
+    return HTMLResponse(
+        content=re.sub(r"{{ content }}", f"<ul>{routes}</ul>", template)
+    )
 
 
 @app.get("/ratelimit")
