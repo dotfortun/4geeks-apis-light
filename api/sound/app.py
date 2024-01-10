ec9478248f4f429a933ee0d53f69fec38c4a8f2f
Shane B <dotfortun@users.noreply.github.com>
2024-01-09 20:58:12 -0500

    First stab at sound API. (#5)
    * Model delete working correctly now.
    
    * Both apps tested and passing.
    
    * Messed up the migration somehow.
    
    * Nope, just had a weird commit issue.
    
    * Trimming out dev requirements.
    
    * First stab at sound API.
    
    * Renamed hello_world to get_all_data

---
+++ api/sound/app.py
@@ -0,0 +1,88 @@
+import json
+from typing import List, Optional, Annotated
+
+from fastapi import (
+    FastAPI, Request, Response, HTTPException,
+    Query, Depends, Path, status,
+)
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
+
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+from sqlmodel import (
+    Session, select
+)
+
+from api.sound.models import (
+    Song, Songs,
+    FX, FXs,
+    SoundData,
+)
+from api.db import get_session
+
+app = FastAPI(
+    title="Sound API",
+    description="An API that you should describe.",
+    docs_url=None,
+)
+
+limiter = Limiter(key_func=get_remote_address)
+# Limiter requires the request to be in the args for your routes!
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.mount("/files", StaticFiles(directory="api/sound/files"), name="files")
+
+data = {
+    "fx": None,
+    "songs": None,
+}
+
+with (
+    open("api/sound/data/fx.json", "rt") as fx_file,
+    open("api/sound/data/songs.json", "rt") as song_file
+):
+    data["fx"] = json.load(fx_file)
+    data["songs"] = json.load(song_file)
+
+
+@app.get("/docs", include_in_schema=False)
+async def swagger_ui_html():
+    return get_swagger_ui_html(
+        title="4Geeks Playground - Sound API",
+        openapi_url="/sound/openapi.json",
+        swagger_favicon_url="/favicon.ico",
+    )
+
+
+@app.get(
+    "/effects",
+    response_model=FXs
+)
+def get_all_fx():
+    return {
+        "sound_effects": data["fx"]
+    }
+
+
+@app.get(
+    "/songs",
+    response_model=Songs
+)
+def get_all_music():
+    return {
+        "songs": data["songs"]
+    }
+
+
+@app.get(
+    "/all",
+)
+@limiter.limit("15/minute")
+def get_all_data(
+    request: Request,
+    session: Session = Depends(get_session)
+) -> None:
+    return data
---
+++ api/sound/test_app.py
@@ -0,0 +1,41 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlmodel import (
+    Session, SQLModel, create_engine
+)
+from sqlalchemy.pool import StaticPool
+
+from api.db import get_session
+from api.sound.app import app
+
+
+@pytest.fixture(name="session")
+def session_fixture():
+    engine = create_engine(
+        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    def get_session_override():
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+
+def test_hello_world(client: TestClient):
+    response = client.get(
+        "/hello"
+    )
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data["message"] == "Hello, world!"
+
