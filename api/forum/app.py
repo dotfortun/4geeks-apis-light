19c351cf8bd94a214e354fb47661b087c8046a5b
Shane B <shane.bell.dev@gmail.com>
2024-01-22 00:58:25 -0500

    So yeah, they introduced a bug back in at some point.

--- api/forum/app.py
+++ api/forum/app.py
@@ -72,7 +72,8 @@ async def scalar_ui_html():
       var apiReference = document.getElementById('api-reference')
       apiReference.dataset.configuration = JSON.stringify(configuration)
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference@1.13.12/dist/browser/standalone.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference@1.8.0/dist/browser/standalone.min.js"></script>
+    # <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference@1.13.12/dist/browser/standalone.min.js"></sceript>
   </body>
 </html>
 """
