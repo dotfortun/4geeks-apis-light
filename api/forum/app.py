61f6839a8b2c579ae768fb91ac9bce7bd9c96ee9
Shane B <shane.bell.dev@gmail.com>
2024-01-22 00:45:48 -0500

    Trying scalar docs on heroku.

--- api/forum/app.py
+++ api/forum/app.py
@@ -42,3 +42,38 @@ async def swagger_ui_html():
         openapi_url="/forum/openapi.json",
         swagger_favicon_url="/favicon.ico",
     )
+
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_ui_html():
+    return Response(
+        content=f"""
+<!doctype html>
+<html>
+  <head>
+    <title>4Geeks Playground - Forum API</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1" />
+  </head>
+  <body style="margin: 0">
+    <script
+      id="api-reference"
+      data-url="/forum/openapi.json"
+    ></script>
+    <!-- You can also set a full configuration object like this -->
+    <!-- easier for nested objects -->
+    <script>
+      var configuration = {'''{
+        theme: 'deepSpace',
+      }'''}
+
+      var apiReference = document.getElementById('api-reference')
+      apiReference.dataset.configuration = JSON.stringify(configuration)
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+  </body>
+</html>
+"""
+    )
