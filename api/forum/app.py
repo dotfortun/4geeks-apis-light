759584348dcd167e900233503ce56c29463dc617
Shane B <shane.bell.dev@gmail.com>
2024-01-22 01:03:15 -0500

    sceript

--- api/forum/app.py
+++ api/forum/app.py
@@ -73,7 +73,7 @@ async def scalar_ui_html():
       apiReference.dataset.configuration = JSON.stringify(configuration)
     </script>
     <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference@1.8.0/dist/browser/standalone.min.js"></script>
-    # <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference@1.13.12/dist/browser/standalone.min.js"></sceript>
+    # <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference@1.13.12/dist/browser/standalone.min.js"></script>
   </body>
 </html>
 """
